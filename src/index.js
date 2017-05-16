45671e83d8119549b91f5dc7ce624fa882dbc7ac
Jeffrey Yan <jeff3yan@gmail.com>
2017-05-16 18:37:57 +1200

    Fix deprecation message

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const deprecate = require('depd')('loopback-find')
+const deprecate = require('depd')('loopback-find-mixin')
 const loopbackFind = require('./find')
 
 module.exports = function mixin(app) {
   app.loopback.modelBuilder.mixins.define = deprecate.function(app.loopback.modelBuilder.mixins.define,
     'app.modelBuilder.mixins.define: Use mixinSources instead ' +
-    'see https://github.com/jeff3yan/loopback-find')
+    'see https://github.com/jeff3yan/loopback-find-mixin')
   app.loopback.modelBuilder.mixins.define('Find', loopbackFind);
 }
